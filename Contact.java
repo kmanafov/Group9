1c5aba727a9de053d3d193b7d7d6ac9545f70ec4
kmanafov <151184257+kmanafov@users.noreply.github.com>
2025-07-13 23:27:11 +0400

    Updated version of Contact

--- Contact.java
+++ Contact.java
@@ -5,19 +5,44 @@ public class Contact {
     private String address;
     private String birthDate;
     private String group;
+    private Company company;  
 
-    public Contact(String fullName, String phoneNumber, String email, String address, String birthDate, String group) {
+    public Contact(String fullName, String phoneNumber, String email, String address,
+                   String birthDate, String group, Company company) {
         this.fullName = fullName;
         this.phoneNumber = phoneNumber;
         this.email = email;
         this.address = address;
         this.birthDate = birthDate;
         this.group = group;
+        this.company = company;
     }
 
+   
+    public String getFullName() { return fullName; }
+    public String getPhoneNumber() { return phoneNumber; }
+    public String getEmail() { return email; }
+    public String getAddress() { return address; }
+    public String getBirthDate() { return birthDate; }
+    public String getGroup() { return group; }
+    public Company getCompany() { return company; }
+
+  
+    public void setPhoneNumber(String phoneNumber) { this.phoneNumber = phoneNumber; }
+    public void setEmail(String email) { this.email = email; }
+    public void setAddress(String address) { this.address = address; }
+    public void setBirthDate(String birthDate) { this.birthDate = birthDate; }
+    public void setGroup(String group) { this.group = group; }
+    public void setCompany(Company company) { this.company = company; }
+
+    @Override
     public String toString() {
-        return "Name: " + fullName + ", Phone: " + phoneNumber + ", Email: " + email +
-                ", Address: " + address + ", Birth Date: " + birthDate + ", Group: " + group;
+        return "Name: " + fullName +
+               " | Phone: " + phoneNumber +
+               " | Email: " + email +
+               " | Address: " + address +
+               " | Birth Date: " + birthDate +
+               " | Group: " + group +
+               " | Company: " + company;
     }
 }
-
