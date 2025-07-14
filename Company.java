2e9599cdafe135bf25f1f997d544c42d5c31ebb2
kmanafov <151184257+kmanafov@users.noreply.github.com>
2025-07-14 14:34:23 +0400

    setters added

--- Company.java
+++ Company.java
@@ -7,13 +7,10 @@ public class Company {
         this.department = department;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
+    public String getName() { return name; }
+    public String getDepartment() { return department; }
+    public void setName(String name) { this.name = name; }
+    public void setDepartment(String department) { this.department = department; }
 
     @Override
     public String toString() {
